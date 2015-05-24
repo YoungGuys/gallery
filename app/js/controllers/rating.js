6919b502e0a65ff64c11516a47256c54755982d3
Коля <kolka.koval@gmail.com>
2015-05-24 17:30:39 +0300

    add visibility rating

--- app/js/controllers/rating.js
+++ app/js/controllers/rating.js
@@ -5,23 +5,51 @@ artApp.controller('ratingCtrl',['$scope','$http', '$location', function($scope, 
     $scope.currentDate = new Date();
 
 
-    $http.get('api/get/alljury')
+    $http.get('api/get/ratingVisibility')
         .success(function(data, status, headers, config) {
-            console.log('\nAll jury');
+            console.log('\nratingVisibility');
             console.log(data);
-            $scope.allJury = data;
+
+            $scope.ratingVisibility = (data == "false") ? false : true;
+
+            if (data) apiData();
         });
 
 
-    $http.get('api/get/projects', {params: null})
-        .success(function(data, status, headers, config) {
-            console.log('\nProjects');
-            console.log(data);
+    $scope.showRating = function () {
+        $scope.ratingVisibility = $scope.ratingVisibility ? 0 : 1;
 
-            $scope.projects = data;
+        $http.get(
+            'api/put/settings',
+            {params: {"ratingVisibility": $scope.ratingVisibility}}
+        )
+            .success(function(data, status, headers, config) {
+                console.log('\nRating setting');
+                console.log(data);
 
-        })
+                apiData();
+            });
 
+    };
+
+
+    function apiData() {
+        $http.get('api/get/alljury')
+            .success(function(data, status, headers, config) {
+                console.log('\nAll jury');
+                console.log(data);
+                $scope.allJury = data;
+            });
+
+
+        $http.get('api/get/projects', {params: null})
+            .success(function(data, status, headers, config) {
+                console.log('\nProjects');
+                console.log(data);
+
+                $scope.projects = data;
+            });
+    }
 
 
 }]);
