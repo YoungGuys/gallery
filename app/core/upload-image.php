322189540c9299536f66aaac1958e14c9bce6e67
andrii <gunter0104@gmail.com>
2015-05-30 00:28:38 +0300

    Change name file on upload

--- app/core/upload-image.php
+++ app/core/upload-image.php
@@ -1,6 +1,6 @@
 <?php
 
-  $filename = $_FILES['file']['name'];
+  $filename = $_POST['fileName'];//$_FILES['file']['name'];
   $destination = '../images/img/' . $filename;
   move_uploaded_file( $_FILES['file']['tmp_name'] , $destination );
 
